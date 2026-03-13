6d69a9a9671b79fb7da42401acb7d932bf62322c
Andrii Derkach <andrijderkac367@gmail.com>
2026-03-13 18:26:50 +0100

    Lab3: Django polls app with admin customization, tests and voting logic

--- Python/backend/manage.py
+++ Python/backend/manage.py
@@ -2,6 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
+
+CURRENT_DIR = Path(__file__).resolve().parent
+PARENT_DIR = CURRENT_DIR.parent
+if str(PARENT_DIR) not in sys.path:
+    sys.path.insert(0, str(PARENT_DIR))
 
 
 def main():
